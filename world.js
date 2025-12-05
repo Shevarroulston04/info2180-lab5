297e99d262a1702654f54cefe8dc0253c1260dbb
Shevar Roulston <shevarroulston04@gmail.com>
2025-12-05 15:04:35 -0500

    Added images for CSS and Completed Exercise 4

--- world.js
+++ world.js
@@ -1,12 +1,21 @@
 window.onload = function() {
     let button = document.querySelector("#lookup");
+    let input = document.querySelector("#country");
 
+    // When clicking the button
     button.addEventListener("click", () => {
-        let country = document.querySelector("#country").value;
+        let country = input.value;
         fetch(`world.php?country=${country}`)
             .then(response => response.text())
             .then(data => {
                 document.querySelector("#result").innerHTML = data;
             });
+    });
+
+    // When pressing Enter
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            button.click();
+        }
     });
 };
--- world.php
+++ world.php
@@ -17,11 +17,22 @@ $stmt = $conn->query($query);
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<table>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>";
 
+foreach ($results as $row) {
+    echo "<tr>
+            <td>{$row['name']}</td>
+            <td>{$row['continent']}</td>
+            <td>{$row['independence_year']}</td>
+            <td>{$row['head_of_state']}</td>
+          </tr>";
+}
 
+echo "</table>";
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
